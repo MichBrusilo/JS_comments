fa30e1f00059ef84d81b3d3f603cc85dbcf1dc23
Michał Brusiło <mibru@interia.pl>
2018-04-03 14:40:20 +0200

    Add index and js files

---
+++ js/scripts.js
@@ -0,0 +1,7 @@
+// scripts.js
+// pierwszy skrypt plus komentarze
+var name = prompt('Enter your name');
+// tutaj będzie alert
+alert('Hello, ' + name);
+// poniżej będzie console.log
+console.log('Hello, ' + name);
